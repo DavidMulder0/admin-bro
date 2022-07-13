6be3979eba0e53285b6da1abc17a388f2a3e5997
DMR9 <david.mulder.contractor@3ds.com>
2022-07-13 08:45:48 +0100

    updates

--- src/frontend/interfaces/action/build-action-click-handler.ts
+++ src/frontend/interfaces/action/build-action-click-handler.ts
@@ -8,38 +8,44 @@ import { ActionJSON } from './action-json.interface'
 import { buildActionCallApiTrigger } from './build-action-api-call-trigger'
 
 export type BuildActionClickOptions = {
-  action: ActionJSON;
-  params: DifferentActionParams;
-  actionResponseHandler: ReturnType<typeof useActionResponseHandler>;
-  push: (path: string, state?: any) => void;
+    action: ActionJSON
+    params: DifferentActionParams
+    actionResponseHandler: ReturnType<typeof useActionResponseHandler>
+    push: (path: string, state?: any) => void
 }
 
 export type BuildActionClickReturn = (event: any) => any
 
 export const buildActionClickHandler = (
-  options: BuildActionClickOptions,
+    options: BuildActionClickOptions
 ): BuildActionClickReturn => {
-  const { action, params, actionResponseHandler, push } = options
+    const { action, params, actionResponseHandler, push } = options
 
-  const handleActionClick = (event: React.MouseEvent<HTMLElement>): void => {
-    event.preventDefault()
-    event.stopPropagation()
+    const handleActionClick = (event: React.MouseEvent<HTMLElement>): void => {
+        event.preventDefault()
+        event.stopPropagation()
 
-    const href = actionHref(action, params)
+        const href = actionHref(action, params)
 
-    const callApi = buildActionCallApiTrigger({
-      params, action, actionResponseHandler,
-    })
+        const callApi = buildActionCallApiTrigger({
+            params,
+            action,
+            actionResponseHandler,
+        })
 
-    if (action.guard && !confirm(action.guard)) {
-      return
+        if (action.guard && !confirm(action.guard)) {
+            return
+        }
+        if (actionHasComponent(action)) {
+            callApi()
+        } else if (href) {
+            if (event.ctrlKey) {
+                window.open(href)
+                return
+            }
+            push(href)
+        }
     }
-    if (actionHasComponent(action)) {
-      callApi()
-    } else if (href) {
-      push(href)
-    }
-  }
 
-  return handleActionClick
+    return handleActionClick
 }
